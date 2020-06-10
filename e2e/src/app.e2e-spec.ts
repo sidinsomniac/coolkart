0377e40e6c6dd00d3606aec5fb705436b324497a
sidinsomniac <chatterjeesid1993@gmail.com>
2020-06-10 21:33:25 +0530

    Setup coolkart

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to coolcart!');
+    expect(page.getTitleText()).toEqual('Welcome to coolkart!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'coolcart'`, () => {
+  it(`should have as title 'coolkart'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('coolcart');
+    expect(app.title).toEqual('coolkart');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to coolcart!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to coolkart!');
   });
 });
