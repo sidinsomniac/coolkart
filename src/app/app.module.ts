5feb342aecd2cc1c50d2c96eac0a7069cf8570e0
sidinsomniac <chatterjeesid1993@gmail.com>
2020-06-11 17:09:37 +0530

    Setup Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppRoutingModule, routingComponents } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,8 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
     NgbModule
   ],
   providers: [],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCkGMFbT5VH_R2XtR2bfZ9FOD9IuoHHG18",
+    authDomain: "coolkart-3f311.firebaseapp.com",
+    databaseURL: "https://coolkart-3f311.firebaseio.com",
+    projectId: "coolkart-3f311",
+    storageBucket: "coolkart-3f311.appspot.com",
+    messagingSenderId: "872533134063",
+    appId: "1:872533134063:web:4d57cd4fe78b0f8467691a"
+  }
 };
 
 /*
